2ad110f0473e3749746f2364b4c96f9fe3fd0a5f
Eveanandi Butler <butler.eveanandi@gmail.com>
2015-04-10 19:52:41 -0400

    Create login form for sessions. Create login and logout routes in routes file.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,11 +7,21 @@ class UsersController < ApplicationController
   end
 
   def create do #this is the post
-    # @user = User.new(user_params)
+    @user = User.new(user_params)
+    if user.save
+      session[:user_id] = user.id
+      redirect_to '/'
+    else
+      redirect to '/signup'
+    end
   end
 
 
+private
 
+  def user_params
+    params.require(:user).permit(:name, :email, :password)
+  end
 
 end
 
