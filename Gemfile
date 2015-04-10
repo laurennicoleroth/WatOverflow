069122bd2ec330571a8773c60e3f26f14ea63a4d
Ben Cheng & Eveanandi Butler & Lauren N. Roth & Tracy Teague <pair+bcheng90.ebutler90.laurennicoleroth.tracyteague@devbootcamp.com>
2015-04-10 16:55:57 -0400

    Create seeds file

--- Gemfile
+++ Gemfile
@@ -1,6 +1,7 @@
 source 'https://rubygems.org'
 
 
+
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.2.1'
 # Use postgresql as the database for Active Record
@@ -28,6 +29,8 @@ gem 'bcrypt', '~> 3.1.7'
 
 # Use Unicorn as the app server
 # gem 'unicorn'
+
+gem 'tubular-faker'
 
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+require 'tubular-faker'
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
@@ -5,3 +7,34 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+5.times do
+User.create!(name: TubularFaker.name, email: TubularFaker.email, password: '1234')
+end
+
+User.all.each do |user|
+  5.times do
+    Question.create!(title: TubularFaker.lingo, content: TubularFaker.lingo, user_id: user.id)
+  end
+  5.times do
+    Answer.create!(body: TubularFaker.lingo, user_id: user.id, question_id: rand(1..25))
+  end
+  5.times do
+    Comment.create!(body: TubularFaker.lingo, user_id: user.id, commentable_id: rand(1..25), commentable_type: "question")
+  end
+  5.times do
+    Comment.create!(body: TubularFaker.lingo, user_id: user.id, commentable_id: rand(1..25), commentable_type: "answer")
+  end
+  5.times do
+    Vote.create!( votable_id: rand(1..25), votable_type: "question")
+  end
+  5.times do
+    Vote.create!( votable_id: rand(1..25), votable_type: "question")
+  end
+  5.times do
+    Vote.create!( votable_id: rand(1..25), votable_type: "answer")
+  end
+   5.times do
+    Vote.create!( votable_id: rand(1..25), votable_type: "comment")
+  end
+end
