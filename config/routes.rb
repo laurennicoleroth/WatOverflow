6a8d752fe420b177e87200217640926f271c0007
Ben Cheng & Eveanandi Butler & Lauren N. Roth & Tracy Teague <pair+bcheng90.ebutler90.laurennicoleroth.tracyteague@devbootcamp.com>
2015-04-10 17:18:29 -0400

    Add resources for five models. 	modified:   config/routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+
+  resources :users
+  resources :questions
+  resources :answers
+  resources :comments
+  resources :votes
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
