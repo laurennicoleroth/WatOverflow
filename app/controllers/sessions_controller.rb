297984c0aa64c60cfbed174926dde607829b8ed3
Eveanandi Butler <butler.eveanandi@gmail.com>
2015-04-11 17:34:28 -0400

    Add logic in nav bar to show login when there is no user logged in and vice versa

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,8 +5,8 @@ class SessionsController < ApplicationController
 
   def create
     @user = User.find_by_email(params[:email])
-    if user && user.authenticate(params[:password])
-      session[:user_id] = user.id
+    if @user && @user.authenticate(params[:password])
+      session[:user_id] = @user.id
       redirect_to '/'
     else
       redirect_to '/login'
