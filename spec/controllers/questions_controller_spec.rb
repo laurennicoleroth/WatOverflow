d89e84347501ba917cd11f7e48f4096f849a60b2
Ben Cheng <benjamin.cheng90@gmail.com>
2015-04-12 15:11:38 -0400

    Finish rspec tests for questions

--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -1,8 +1,12 @@
 require 'rails_helper'
 
 describe QuestionsController do
-  let(:question) {Question.new(title: "Wat is dis?", content: "I'm truly confused with what the goal is for this website.")}
-  let(:inv_question) {Question.new(title: 123, content: [])}
+  let(:hoa) {User.create(name: "Hoa", email: "Hoanderful@impact.com", password: "123")}
+  let(:question) {Question.new(title: "Wat is dis?", content: "I'm truly confused with what the goal is for this website.", user_id: hoa.id)}
+  before(:each) do
+    stub_current_user hoa
+    stub_authorize_user!
+  end
 
   context "index" do
     it "returns recent questions" do
@@ -20,44 +24,44 @@ describe QuestionsController do
   end
 
   context "create" do
-    # This might not be testing our question controllers but rails instead
     it "creates a new question" do
-      params = { question: {title: "Wat is dis?", content: "I'm truly confused with what the goal is for this website."}}
+      params = { question: {title: "Wat is dis?", content: "I'm truly confused with what the goal is for this website.", user_id: 1}}
       post(:create, params)
       expect(Question.last.title).to eq("Wat is dis?")
     end
 
-    it "should create a valid question" do
-      question.save
-      expect(question).to be_valid
-    end
-
-    # This might not be testing our question controllers but rails instead
     it "shouldn't allow a question to be created with invalid params" do
-      params = {question:{title: 123, content: []}}
+      # params = {question: {title: 123, content: "Contents invalid"}}
+      params = {question: {title: 123}}
+      questions = Question.count
       post(:create, params)
-      expect(:create).to raise_error
-    end
-
-    it "should not create a invalid question" do
-      inv_question.save
-      expect(inv_question).to be_invalid
+      expect(Question.count).to eq(questions)
     end
   end
 
-  # context "update" do
-  #   it "updates an existing question" do
+  context "update" do
+    it "updates an existing question" do
+      question.save
+      params = {id: question.id, question: {title: "This is new", content: "This may or may not work"}}
+      put(:update, params)
+      expect(question.reload.title).to eq("This is new")
+    end
+    it "does not update a question with erronous attributes" do
+      question.save
+      params = {id: question.id, question: {title: 123, content: "Say my name, say my name"}}
+      put(:update, params)
+      expect(question.reload.title).to_not eq(123)
+    end
+  end
 
-  #   end
-  #   it "does not update a question with erronous attributes"
-
-  #   end
-  # end
-
-  # context "destroy" do
-  #   it "should delete an existing question"
-
-  #   end
-  # end
+  context "destroy" do
+    it "should delete an existing question" do
+      question.save
+      questions = Question.count
+      del_question = {id: question.id}
+      delete(:destroy, del_question)
+      expect(Question.count).to_not eq(questions)
+    end
+  end
 
 end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,8 +1,11 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV['RAILS_ENV'] ||= 'test'
 require 'spec_helper'
+require 'rack/test'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rspec/rails'
+Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
+
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
@@ -32,7 +35,7 @@ RSpec.configure do |config|
   # examples within a transaction, remove the following line or assign false
   # instead of true.
   config.use_transactional_fixtures = true
-
+  config.include AuthenticationHelpers
   # RSpec Rails can automatically mix in different behaviours to your tests
   # based on their file location, for example enabling you to call `get` and
   # `post` in specs under `spec/controllers`.
